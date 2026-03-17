562f1a1e5e54948eb7762231d748a931b3c4e88e
carlomazzanti <carlomazzanti00@gmail.com>
2026-03-17 14:38:29 +0100

    springboot

---
+++ java/dsmt/src/main/java/it/unipi/dsmt/DsmtApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.dsmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmtApplication.class, args);
+	}
+
+}
---
+++ java/dsmt/src/test/java/it/unipi/dsmt/DsmtApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.dsmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
