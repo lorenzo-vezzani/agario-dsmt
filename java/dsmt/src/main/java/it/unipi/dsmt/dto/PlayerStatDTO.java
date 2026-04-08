692d16b23544c8f50cdd43ba9939029b575c5370
carlomazzanti <carlomazzanti00@gmail.com>
2026-04-08 11:29:45 +0200

    kills, deaths -> k, d in json

--- java/dsmt/src/main/java/it/unipi/dsmt/dto/PlayerStatDTO.java
+++ java/dsmt/src/main/java/it/unipi/dsmt/dto/PlayerStatDTO.java
@@ -1,7 +1,14 @@
 package it.unipi.dsmt.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PlayerStatDTO {
+    @JsonProperty("id")
     public String id;
+
+    @JsonProperty("k")
     public int kills;
+
+    @JsonProperty("d")
     public int deaths;
 }
